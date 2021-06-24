4214f1143d0923c91e61c1d1069a01f718bcdc03
DreadfulCrazy <lucasospina97@gmail.com>
2021-06-24 00:22:55 -0500

    Subscription newComment implemented

--- client/src/components/Modal/ModalPublication/Comments/Comments.js
+++ client/src/components/Modal/ModalPublication/Comments/Comments.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Image } from "semantic-ui-react";
 import { map } from "lodash";
-import { GET_COMMENTS } from "../../../../gql/comment";
-import { useQuery } from "@apollo/client";
+import { GET_COMMENTS, WS_GET_COMMENTS } from "../../../../gql/comment";
+import { useQuery, useSubscription, useApolloClient } from "@apollo/client";
 import { Link } from "react-router-dom";
 import ImageNotFound from "../../../../assets/png/avatar.png";
 import "./Comments.scss";
 
 const Comments = ({ publication }) => {
+  const [comments, setComments] = useState([]);
+  const client = useApolloClient();
   const { data, loading } = useQuery(GET_COMMENTS, {
     variables: {
       id: publication.id,
     },
   });
+
+  const { data: dataWs } = useSubscription(WS_GET_COMMENTS, {
+    variables: {
+      id: publication.id,
+    },
+  });
+  useEffect(() => {
+    setComments(data?.getComments);
+  }, [data]);
+
+  useEffect(() => {
+    if (dataWs?.newComment) {
+      client.writeQuery({
+        query: GET_COMMENTS,
+        data: {
+          getComments: { ...dataWs.newComment },
+        },
+        variables: {
+          id: publication.id,
+        },
+      });
+    }
+  }, [dataWs, publication, client]);
+
   if (loading) return null;
-  const { getComments } = data;
+
   return (
     <div className="comments">
-      {map(getComments, (comment, index) => (
-        <Link to={`/${comment.idUser.username}`} className="comment">
+      {map(comments, (comment, index) => (
+        <Link
+          to={`/${comment.idUser.username}`}
+          className="comment"
+          key={index}
+        >
           <Image src={comment.idUser.avatar || ImageNotFound} avatar />
           <div>
             <p>{comment.idUser.username}</p>
--- client/src/gql/comment.js
+++ client/src/gql/comment.js
@@ -23,3 +23,18 @@ export const GET_COMMENTS = gql`
     }
   }
 `;
+
+export const WS_GET_COMMENTS = gql`
+  subscription newComment($id: ID) {
+    newComment(id: $id) {
+      idUser {
+        username
+        avatar
+        name
+      }
+      comment
+      createdAt
+      idPublication
+    }
+  }
+`;
