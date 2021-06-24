8500a342a83c6d48bbb86bc838f2d2ba2f5ab6cc
DreadfulCrazy <lucasospina97@gmail.com>
2021-06-23 21:21:45 -0500

    Mutation addComment implemented

--- client/src/components/Modal/ModalPublication/CommentForm/CommentForm.js
+++ client/src/components/Modal/ModalPublication/CommentForm/CommentForm.js
@@ -1,11 +1,47 @@
 import React from "react";
-import "./CommentForm.scss";
+import { useMutation } from "@apollo/client";
 import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import "./CommentForm.scss";
+import { ADD_COMMENT } from "../../../../gql/comment";
 
 function CommentForm({ publication }) {
+  const [addComment] = useMutation(ADD_COMMENT);
+
+  const { handleSubmit, values, handleChange, errors, handleReset } = useFormik(
+    {
+      initialValues: {
+        comment: "",
+      },
+      validationSchema: Yup.object({
+        comment: Yup.string().required(),
+      }),
+      onSubmit: async ({ comment }) => {
+        try {
+          let result = await addComment({
+            variables: {
+              input: { idPublication: publication.id, comment },
+            },
+          });
+          console.log(result);
+          handleReset();
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }
+  );
+
   return (
-    <Form className="comment-form">
-      <Form.Input placeholder="Anade un comentario..." name="comment" />
+    <Form className="comment-form" onSubmit={handleSubmit}>
+      <Form.Input
+        placeholder="Anade un comentario..."
+        name="comment"
+        value={values.comment}
+        onChange={handleChange}
+        error={errors.comment && true}
+      />
       <Button type="submit">Publicar</Button>
     </Form>
   );
